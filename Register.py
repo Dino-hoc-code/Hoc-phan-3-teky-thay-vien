275b1e9f5f51b71fe2eab7104a55ce9bc7e6d2c2
Dino-hoc-code <dinosaurgamingytb@gmail.com>
2024-09-14 16:41:36 +0700

    Update Register.py

--- Register.py
+++ Register.py
@@ -10,26 +10,30 @@ class MyProject:
 
         self.x = 10
         self.y = 10
+        self.Width = 50
 
         self.lblUsername = tk.Label(self.root, text='Your Username')
         self.lblUsername.place(x=self.x, y=self.y)
-        self.etyUsername = tk.Entry(self.root)
+        self.etyUsername = tk.Entry(self.root, width= self.Width)
         self.etyUsername.place(x=self.x, y=self.y + 20)
 
         self.lblEmail = tk.Label(self.root, text='Your Email')
         self.lblEmail.place(x=self.x, y=self.y + 40)
-        self.etyEmail = tk.Entry(self.root)
+        self.etyEmail = tk.Entry(self.root, width= self.Width)
         self.etyEmail.place(x=self.x, y=self.y + 60)
 
         self.lblPassword = tk.Label(self.root, text='Your Password')
         self.lblPassword.place(x=self.x, y=self.y + 80)
-        self.etyPassword = tk.Entry(self.root)
+        self.etyPassword = tk.Entry(self.root, width= self.Width)
         self.etyPassword.place(x=self.x, y=self.y + 100)
 
         self.lblCheckPassword = tk.Label(self.root, text='Check Your Password')
         self.lblCheckPassword.place(x=self.x, y=self.y + 120)
-        self.etyCheckPassword = tk.Entry(self.root)
+        self.etyCheckPassword = tk.Entry(self.root, width= self.Width)
         self.etyCheckPassword.place(x=self.x, y=self.y +140)
+
+        self.btnRegister = tk.Button(self.root, text= 'Register' )
+        self.btnRegister.place(x=self.x + 200, y=self.y + 180)
 
 # Chạy ứng dụng
 if __name__=='__main__':
